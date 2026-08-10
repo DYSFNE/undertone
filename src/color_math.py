f3d212704d947bfceb832ae1a9e9a044f3a277d6
dysfne <dysfne@gmail.com>
2026-08-10 00:22:37 -0400

    chore: add color_math.py
    The file was added as the first color science math file to the project

--- src/color_math.py
+++ src/color_math.py
@@ -1,10 +1,18 @@
-from typing import NewType
+'''
+color_math.py
+'''
 
-RGB8 = tuple[int, int, int]
-SRGB = NewType("SRGB", tuple[float, float, float])
-LinearRGB = NewType("LinearRGB", tuple[float, float, float])
-XYZ = NewType("XYZ", tuple[float, float, float])
-LinearLMS = NewType("LinearLMS", tuple[float, float, float])
+from typing import NewType
+import math
+
+RGB8            = NewType("RGB8", tuple[int, int, int])
+SRGB            = NewType("SRGB", tuple[float, float, float])
+LinearRGB       = NewType("LinearRGB", tuple[float, float, float])
+XYZ             = NewType("XYZ", tuple[float, float, float])
+LinearLMS       = NewType("LinearLMS", tuple[float, float, float])
+NonLinearLMS    = NewType("NonLinearLMS", tuple[float, float, float])
+Oklab           = NewType("Oklab", tuple[float, float, float])
+OKLCH           = NewType("OKLCH", tuple[float, float, float])
 
 MAX_CHANNEL_VALUE = 255
 
@@ -13,6 +21,8 @@ SRGB_LINEAR_THRESHOLD = 0.04045
 SRGB_LINEAR_SLOPE = 12.92
 SRGB_OFFSET = 0.055
 SRGB_EXPONENT = 2.4
+
+CHROMA_EPSILON = 1e-8
 
 _HEX_DIGITS = frozenset("0123456789abcdefABCDEF")
 
@@ -104,20 +114,20 @@ def xyz_to_linear_rgb(xyz: XYZ) -> LinearRGB:
     x, y, z = xyz
 
     red = (
-        3.2409699419045213 * x 
-        - 1.5373831775700935 * y 
+        3.2409699419045213 * x
+        - 1.5373831775700935 * y
         - 0.4986107602930033 * z
     )
 
     green = (
-        -0.9692436362808798 * x 
-        + 1.8759675015077206 * y 
+        -0.9692436362808798 * x
+        + 1.8759675015077206 * y
         + 0.0415550574071756 * z
     )
 
     blue = (
-        0.0556300796969936 * x 
-        - 0.2039769588889765 * y 
+        0.0556300796969936 * x
+        - 0.2039769588889765 * y
         + 1.0569715142428786 * z
     )
 
@@ -133,7 +143,55 @@ def xyz_to_linear_lms(xyz: XYZ) -> LinearLMS:
         - 0.1288597137 * z
     )
 
-    medium = 
+    medium = (
+        0.0329845436 * x
+        + 0.9293118715 * y
+        + 0.0361456387 * z
+    )
 
-    small = 
+    small = (
+        0.0482003018 * x
+        + 0.2643414314 * y
+        + 0.6338517070 * z
+    )
 
+    return LinearLMS((long, medium, small))
+
+
+def linear_lms_to_nonlinear(lms: LinearLMS) -> NonLinearLMS:
+    large, medium, small = (channel ** 1/3 for channel in lms)
+    return NonLinearLMS((large, medium, small))
+
+
+def nonlinear_lms_to_oklab(lms: NonLinearLMS) -> Oklab:
+    l, m, s = lms
+
+    lightness = (
+        0.2104542553 * l
+        + 0.7936177850 * m
+        - 0.0040720402 * s
+    )
+
+    green_red = (
+        1.9779984951 * l
+        - 2.4285922050 * m
+        + 0.4505937099 * s
+    )
+
+    blue_yellow = (
+        0.0259040371 * l
+        + 0.7827717662 * m
+        - 0.8086757660 * s
+    )
+
+    return Oklab((lightness, green_red, blue_yellow))
+
+
+def oklab_to_oklch(oklab_rectangle: Oklab) -> OKLCH:
+    l, a, b = oklab_rectangle
+
+    chroma = math.hypot(a, b)
+    if chroma < CHROMA_EPSILON:
+        return OKLCH((l, 0.0, 0.0))
+    hue = math.degrees(math.atan2(b, a)) % 360
+    return OKLCH((l, chroma, hue))
